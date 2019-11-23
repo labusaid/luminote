1886fc0d9d3caf7e340999f9cfde4c43dd1bac78
labusaid <labusaid@gmail.com>
2019-11-22 21:43:26 -0600

    playing around with librosa and added some TODOs

--- musicanlysis.py
+++ musicanlysis.py
@@ -5,15 +5,18 @@ import librosa
 
 # TODO: create matrix of equalizer levels
 
-# TODO:
+# TODO: drum separation
 
 # Load the audio as a waveform `y`
 # Store the sampling rate as `sr`
 y, sr = librosa.load(config.music_path)
-y_percussive = librosa.effects.percussive(y)
-y_harmonic = librosa.effects.percussive(y)
-librosa.output.write_wav('music/percussive.wav', y_percussive, sr)
-librosa.output.write_wav('music/harmonic.wav', y_harmonic, sr)
+
+
+
+# y_percussive = librosa.effects.percussive(y)
+# y_harmonic = librosa.effects.percussive(y)
+# librosa.output.write_wav('music/percussive.wav', y_percussive, sr)
+# librosa.output.write_wav('music/harmonic.wav', y_harmonic, sr)
 
 # # Run the default beat tracker
 # tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
