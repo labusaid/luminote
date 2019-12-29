d862676cae1c47c816646305785131f8c70ab4d6
labusaid <labusaid@gmail.com>
2019-12-29 16:29:39 -0600

    setup block templating so individual pages inherit the navbar and other site-wide features

--- index.py
+++ index.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('homepage.html')
 
 @app.route('/setup')
 def setup():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/wiki')
 def wiki():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/resources')
 def css():
